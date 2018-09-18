546f9a84b4bc3dea8b2a2a7d64416dd1dcbe3d81
C.P <cornelie.ploeg@student.hu.nl>
2018-09-18 10:22:27 +0200

    Git committen uitproberen

---
+++ Les4/pe4_3.py
@@ -0,0 +1,4 @@
+uurloon = eval(input("Hoeveel verdien je per uur?"))
+aantaluren = eval(input("Hoeveel uur heb je gewerkt?"))
+totaalverdiend = aantaluren * uurloon
+print(str(aantaluren) + " uur werken levert " + str(totaalverdiend) + " Euro op." )
